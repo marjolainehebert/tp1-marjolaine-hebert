30b8a9143d914c2a281b6bb659595831e3fb65c3
Marjolaine Hébert <marjolaine.hebert@umontreal.ca>
2020-09-13 18:16:36 -0400

    Checkpoint save

---
+++ javascript/fonctions.js
@@ -0,0 +1,9 @@
+
+
+var tableauEntree = new Array("Escargots", "Salade");
+var tableauPlat = new Array("Spaghetti", "Lasagne");
+
+console.log("afficher le contenu du tableau");
+for(var i=0; i<tableauEntree.length;i++){ //i = position dans le tableau
+    console.log(tableauEntree[i]);
+}
