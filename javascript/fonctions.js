76b3e03d374bc299af51e6aed2e27881b69ab9b7
Marjolaine Hébert <marjolaine.hebert@umontreal.ca>
2020-09-14 09:19:55 -0400

    checkpoint save 2020-09-13

--- javascript/fonctions.js
+++ javascript/fonctions.js
@@ -1,9 +1,24 @@
 
+// tableau des entrées
+var entrees=new Array();
+entrees['salade']=new Array('salade.jpg',5);
+entrees['escargots']=new Array('escargots.jpg',25);
 
-var tableauEntree = new Array("Escargots", "Salade");
-var tableauPlat = new Array("Spaghetti", "Lasagne");
+// fonction pour charger le sélecteur 
+function chargerSel(){
+    var selEntree=document.getElementById('selEntree');
+    selEntree.options[0]=new Option("Choisir une entree");
+    var i=1;
+    for (uneEntree in entrees){
+        selEntree.options[i++]=new Option(uneEntree);
+    }
+}
 
-console.log("afficher le contenu du tableau");
-for(var i=0; i<tableauEntree.length;i++){ //i = position dans le tableau
-    console.log(tableauEntree[i]);
+//fonction pour afficher les items selon le choix dans le menu déroulant
+function choixEntree(){
+    var selEntree=document.getElementById('selEntree');
+    var votreEntree=selEntree.options[selEntree.selectedIndex].text;
+    var tab=entrees[votreEntree];
+    document.getElementById('imgEntree').src=tab[0];
+    alert("IMAGE="+tab[0]+"===="+"PRIX="+tab[1]+"$");
 }
