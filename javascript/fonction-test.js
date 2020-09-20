9b1627b0d536461382f421aa591c5d66e2e303c6
Marjolaine Hébert <mhebert@trisotech.com>
2020-09-20 10:44:07 -0400

    finaliser fonction calcul

---
+++ javascript/fonction-test.js
@@ -0,0 +1,12 @@
+var tabEntree=[
+    {"nom":"Salade","prix:5.95","salade.jpg"},
+    {"nom":"Escargot","prix:4.95","escargots.jpg"}
+]
+var tabRepas=[
+    {"nom":"Spaghetti","prix:8.95","spaghetti.jpg"},
+    {"nom":"Lasagne","prix:9.95","lasagne.jpg"}
+]
+
+function remplirSelection() {
+    var selection = document.getElementById('sel')
+}
--- javascript/fonctions.js
+++ javascript/fonctions.js
@@ -1,15 +1,17 @@
 // tableau des entrées
-var sousTotal, taxes, total;
 var entrees=new Array();
     entrees['salade']=new Array('salade.jpg',5.95);
 	entrees['escargots']=new Array('escargots.jpg',4.95);
     
-var plat=new Array()
-    plat['spaghetti']=new Array('spaghetti.jpg', 8.95);
-    plat['lasagne']=new Array('lasagne.jpg', 9.95);
-        
+var repas=new Array()
+	repas['spaghetti']=new Array('spaghetti.jpg', 8.95);
+	repas['lasagne']=new Array('lasagne.jpg', 9.95);
+	
+var prixEntree=0;
+var prixRepas=0;
+    
 // fonction pour charger le sélecteur 
-function chargerSel(){
+function chargerSelEntree(){
 	var selEntree=document.getElementById('selEntree');
 	selEntree.options[0]=new Option("Choisir une entree");
 	var i=1;
@@ -18,11 +20,43 @@ function chargerSel(){
 	}
 }     
 
+function chargerSelRepas(){
+	var selRepas=document.getElementById('selRepas');
+	selRepas.options[0]=new Option("Choisir une entree");
+	var i=1;
+	for (unRepas in repas){
+		selRepas.options[i++]=new Option(unRepas);
+	}
+}  
+
 //fonction pour afficher les items selon le choix du menu déroulant
 function choixEntree(){
 	var selEntree=document.getElementById('selEntree');
 	var votreEntree=selEntree.options[selEntree.selectedIndex].text;
 	var tab=entrees[votreEntree];
-	document.getElementById('imgEntree').src='images/'+tab[0];
-	alert("IMAGE="+tab[0]+"===="+"PRIX="+tab[1]+"$");
+    document.getElementById('imgEntree').src='images/'+tab[0];
+	prixEntree=tab[1];
+	document.getElementById('prixEntree').innerHTML='Prix : '+prixEntree+" $";
+	alert (prixEntree);
+	calculerPrix()
+}
+
+function choixRepas(){
+	var selRepas=document.getElementById('selRepas');
+	var votreRepas=selRepas.options[selRepas.selectedIndex].text;
+	var tab=repas[votreRepas];
+    document.getElementById('imgRepas').src='images/'+tab[0];
+	prixRepas=tab[1];
+	document.getElementById('prixRepas').innerHTML='Prix : '+prixRepas+" $";
+	alert (prixRepas);
+	calculerPrix()
+}
+
+function calculerPrix() {
+	var prixAvantTaxes=prixEntree+prixRepas;
+	var taxes=prixAvantTaxes*14.975/100;
+	var prixTotal=prixAvantTaxes+taxes;
+	document.getElementById('prix-avant-taxes').innerHTML=prixAvantTaxes.toFixed(2)+' $';
+	document.getElementById('les-taxes').innerHTML=taxes.toFixed(2)+' $';
+	document.getElementById('prix-total').innerHTML=prixTotal.toFixed(2)+' $';
 }
